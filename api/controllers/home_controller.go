70c740eff838742d460df3156900ac035ddc9fd5
ndiritumichael <ndiritumichael5@gmail.com>
2020-10-01 19:44:59 +0300

    updating home screen

--- api/controllers/home_controller.go
+++ api/controllers/home_controller.go
@@ -9,5 +9,6 @@ import (
 func (server *Server) Home(w http.ResponseWriter, r *http.Request) {
 
 	responses.JSON(w, http.StatusOK, "Welcome To This Awesome API")
+	responses.JSON(w, http.StatusOK, "to view users  use '/users'")
 
 }
